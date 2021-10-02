4e38c5d9d05fded1e258ba4a869c5fefd1160ddd
adecodess <ademosuu@gmail.com>
2021-10-02 20:52:49 +0100

    added user analytics with charts

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: 'Jan',
+    'Active User': 3000,
+  },
+  {
+    name: 'Feb',
+    'Active User': 5000,
+  },
+  {
+    name: 'Mar',
+    'Active User': 7000,
+  },
+  {
+    name: 'Apr',
+    'Active User': 9000,
+  },
+  {
+    name: 'May',
+    'Active User': 11000,
+  },
+  {
+    name: 'June',
+    'Active User': 13000,
+  },
+  {
+    name: 'July',
+    'Active User': 16000,
+  },
+  {
+    name: 'Aug',
+    'Active User': 18000,
+  },
+  {
+    name: 'Sep',
+    'Active User': 20000,
+  },
+  {
+    name: 'Oct',
+    'Active User': 21000,
+  },
+  {
+    name: 'Nov',
+    'Active User': 8000,
+  },
+  {
+    name: 'Dec',
+    'Active User': 6000,
+  },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Chart from '../../components/chart/Chart';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import { userData } from '../../dummyData';
 import './home.css';
 
 const Home = () => {
   return (
     <main className='home'>
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title='User Analytics'
+        grid
+        dataKey='Active User '
+      />
+      <div className='homeWidgets'></div>
     </main>
   );
 };
