90832085ac1c94e17afaa8afb620f311f3e59490
adecodess <ademosuu@gmail.com>
2021-10-04 23:56:08 +0100

    completed users regsiter page

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -13,6 +13,7 @@ import {
   WorkOutline,
   Report,
 } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
@@ -38,14 +39,18 @@ const Sidebar = () => {
         <div className='sidebarMenu'>
           <h3 className='sidebarTitle'>quick menu</h3>
           <ul className='sidebarList'>
-            <li className='sidebarListItem active'>
-              <PermIdentity className='sidebarIcon' />
-              Users
-            </li>
-            <li className='sidebarListItem'>
-              <Storefront className='sidebarIcon' />
-              Products
-            </li>
+            <Link to='/users' className='link'>
+              <li className='sidebarListItem '>
+                <PermIdentity className='sidebarIcon' />
+                Users
+              </li>
+            </Link>
+            <Link to='/products' className='link'>
+              <li className='sidebarListItem'>
+                <Storefront className='sidebarIcon' />
+                Products
+              </li>
+            </Link>
             <li className='sidebarListItem'>
               <AttachMoney className='sidebarIcon' />
               Transactions
--- src/pages/newUser/NewUser.jsx
+++ src/pages/newUser/NewUser.jsx
@@ -1,7 +1,56 @@
 import './newUser.css';
 
 const NewUser = () => {
-  return <div>new user</div>;
+  return (
+    <main className='newUser'>
+      <h1 className='newUserTitle'>New User</h1>
+      <form className='newUserForm'>
+        <div className='newUserItem'>
+          <label>Username</label>
+          <input type='text' placeholder='John' />
+        </div>
+        <div className='newUserItem'>
+          <label>Full Name</label>
+          <input type='text' placeholder='John Doe' />
+        </div>
+        <div className='newUserItem'>
+          <label>Email</label>
+          <input type='email' placeholder='johndoe@gmail.com' />
+        </div>
+        <div className='newUserItem'>
+          <label>Password</label>
+          <input type='password' placeholder='password' />
+        </div>
+        <div className='newUserItem'>
+          <label>Phone</label>
+          <input type='tel' placeholder='+1 234 456 77' />
+        </div>
+        <div className='newUserItem'>
+          <label>Address</label>
+          <input type='text' placeholder='New York' />
+        </div>
+        <div className='newUserItem'>
+          <label>Gender</label>
+          <div className='newUserGender'>
+            <input type='radio' name='gender' id='male' value='male' />
+            <label for='male'>Male</label>
+            <input type='radio' name='gender' id='female' value='female' />
+            <label for='female'>Female</label>
+            <input type='radio' name='gender' id='others' value='others' />
+            <label for='others'>Others</label>
+          </div>
+        </div>
+        <div className='newUserItem'>
+          <label>Active</label>
+          <select name='active' id='active' className='newUserSelect'>
+            <option value='yes'>Yes</option>
+            <option value='no'>No</option>
+          </select>
+        </div>
+        <button className='newUserButton'>create</button>
+      </form>
+    </main>
+  );
 };
 
 export default NewUser;
